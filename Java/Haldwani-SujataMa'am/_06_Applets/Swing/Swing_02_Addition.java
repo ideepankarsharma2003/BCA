f94f660635382a6e2872df931b35946aa6a74efd
Lucky <deepankarsharma2003@gmail.com>
2021-12-30 22:01:29 +0530

    added calculater made in swing

--- Java/Haldwani-SujataMa'am/_06_Applets/Swing/Swing_02_Addition.java
+++ Java/Haldwani-SujataMa'am/_06_Applets/Swing/Swing_02_Addition.java
@@ -16,20 +16,34 @@ public class Swing_02_Addition extends JFrame implements ActionListener{
 
     Swing_02_Addition(){
         setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLayout(new FlowLayout(FlowLayout.LEFT));
         t1= new JTextField("0", 10);
         t2= new JTextField("0", 10);
 
-        ActionListener al= new ActionListener(){ // ----> Inner Anonymous Class !!!
-            public void actionPerformed(ActionEvent ae){
-                double num1= Double.parseDouble(t1.getText());
-                double num2= Double.parseDouble(t2.getText());
-                double sum= num1+num2;
-                l1.setText(String.valueOf(sum));
-            }
-        }; 
-        b1.addActionListener(al); 
+        /*
+            // Anonyous Class Implementation
+            ActionListener al= new ActionListener(){ // ----> Inner Anonymous Class !!!
+                public void actionPerformed(ActionEvent ae){
+                    double num1= Double.parseDouble(t1.getText());
+                    double num2= Double.parseDouble(t2.getText());
+                    double sum= num1+num2;
+                    l1.setText(String.valueOf(sum));
+                }
+            }; 
+            b1.addActionListener(al);
+        */ 
+
+        // lambda expressions ----> JAVA 8
+        // Functional Interface ----> one method declaration(one method in interface)
+        b1.addActionListener(ae ->{
+            double num1 = Double.parseDouble(t1.getText());
+            double num2 = Double.parseDouble(t2.getText());
+            double sum = num1 + num2;
+            l1.setText(String.valueOf(sum)); 
+        }
+        );
 
 
         // b1.addActionListener(this); // ActionListener is an interface, and 'this' is an anonymous object !!!!
@@ -50,8 +64,7 @@ public class Swing_02_Addition extends JFrame implements ActionListener{
     }
 
     public static void main(String[] args) {
-        Swing_02_Addition obj= new Swing_02_Addition();
-    
+        Swing_02_Addition obj= new Swing_02_Addition();    
     }
     
 }
---
+++ Java/Haldwani-SujataMa'am/_06_Applets/Swing/Swing_03_Calculator.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Swing_03_Calculator extends JFrame implements ActionListener{
+    
+    JTextField t1 = new JTextField("0", 15);
+    JTextField t2 = new JTextField("0", 15);
+    JButton b1 = new JButton("ADD");
+    JButton b2 = new JButton("SUBTRACT");
+    JButton b3 = new JButton("MULTIPLY");
+    JButton b4 = new JButton("DIVIDE");
+    JButton b5 = new JButton("MODULUS");
+    JLabel l = new JLabel("Result");
+
+    Swing_03_Calculator(){
+        setVisible(true);
+        setSize(600, 500);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
+        b5.addActionListener(this);
+
+
+        add(t1);
+        add(t2);
+        add(b1);
+        add(b2);
+        add(b3);
+        add(b4);
+        add(b5);
+        add(l);        
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae){
+        double value=0.0;
+        double num1= Double.parseDouble(t1.getText());
+        double num2= Double.parseDouble(t2.getText());
+
+        if(ae.getSource()==b1){
+            value= num1+num2;
+        }
+        if(ae.getSource()==b2){
+            value= num1-num2;
+        }
+        if(ae.getSource()==b3){
+            value= num1*num2;
+        }
+        if(ae.getSource()==b4){
+            value= num1/num2;
+        }
+        if(ae.getSource()==b5){
+            value= num1%num2;
+        }
+
+        l.setText(String.valueOf(value));
+    }
+
+
+    public static void main(String[] args) {
+        Swing_03_Calculator obj= new Swing_03_Calculator();
+    }
+}
