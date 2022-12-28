437295451a43fefa9a4039dfd2f6256504aaf09c
Lucky <deepankarsharma2003@gmail.com>
2022-12-28 21:22:46 +0530

    added simplified ajax

--- unit05/ajax/updateTextFile.php
+++ unit05/ajax/updateTextFile.php
@@ -1,11 +1,12 @@
 <?php
 
 // get the text from the request
-$text = "This is the modified text , I willl write.........";
+$text = "This is the gazab ka text";
 echo $text;
 
 // open the text file for writing
-$file = fopen("text.txt", "w");
+// $file = fopen("text.txt", "w");
+$file = fopen("gazab.txt", "w");
 
 // write the new text to the file
 fwrite($file, $text);
